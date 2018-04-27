3d6ec08dbdad42ed9bcee365ef88d32947f5b86d
Steve Oldfield <steveold1611@gmail.com>
2018-04-27 13:27:43 -0600

    finished Part-1:

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,12 +1,37 @@
-var health = 100;
+var health = 100
+var name1 = "wacky"
+var hits = 0
 
 
 function slap(){
     health--
+    hits++
+    // alert(health)
+    update()
+}
+
+function punch(){
+    health -= 5
+    hits += 1
+ //   alert(health)
+    update()
+}
+
+function kick(){
+    health -= 10
+    hits += 1
+    // alert(health)
+    update()
+}
+
+function update(){
+    // @ts-ignore
+    document.getElementById('healthNum').innerText = health
+    // @ts-ignore
+    document.getElementById('hithNum').innerText = hits
+    document.getElementById('nameID').innerText = name1
 }
 
 
 
-
-
-slap()
+update()
