e651d2a9ac5e28c94b5f05000ffb82f4841f80eb
Steve Oldfield <steveold1611@gmail.com>
2018-04-27 17:03:24 -0600

    almost done: post notes

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -6,35 +6,41 @@
 var endTarget = new Target('Wacky', 100)
 
 var items = {
-    shield:new Item('Shield',0.3, "This is an awesome shield!"),
+    shield: new Item('Shield', 0.3, "This is an awesome shield!"),
     fist: new Item('fist of thunder', 10, 'will give +10 damage'),
     rock: new Item('Rock', 3, 'just a simple but hard rock')
 }
 
 
-function slap(){
-   // debugger
-    endTarget.health--
-    endTarget.hits++
-    // alert(health)
-    update()
-}
-
-function punch(){
-    endTarget.health -= 5
-    endTarget.hits += 1
- //   alert(health)
-    update()
-}
-
-function kick(){
-    endTarget.health -= 10
+function slap() {
+    // debugger
+    addMods()
+    endTarget.health -= ((modTotal) + 1)  // could also use modtotal() /??????
+    console.log((modTotal) + 1)
     endTarget.hits += 1
     // alert(health)
     update()
 }
 
-function update(){
+function punch() {
+    addMods()
+    endTarget.health -= (modTotal + 5)
+    console.log((modTotal) + 5)
+    endTarget.hits += 1
+    //   alert(health)
+    update()
+}
+
+function kick() {
+    endTarget.health -= (modTotal + 10)
+    console.log((modTotal) + 10)
+    endTarget.hits += 1
+    // alert(health)
+    update()
+}
+
+
+function update() {
     // @ts-ignore
     document.getElementById('healthNum').innerText = endTarget.health
     // @ts-ignore
@@ -42,40 +48,47 @@ function update(){
     document.getElementById('nameID').innerText = endTarget.name
 }
 
-function giveItemToTarget() {
+function giveItemToTarget(item) {
     //bug bug: hard coded shield for now
     //BUG BUG: this could cause some issues later: KEEP EYE ON!!!!
-       endTarget.items.push(items.shield)
-       //bug bug: temp entry
-    //endTarget.items.push(items.rock)
-       //endTarget.items.push(items.fist)
+    debugger
+    endTarget.items.push(items.item)
+    //bug bug: temp entry
+  //  endTarget.items.push(items.rock)
+  //  endTarget.items.push(items.fist)
+
+}
+var modTotal = 0
+function addMods() {
+    modTotal = 0
+    for (let i = 0; i < endTarget.items.length; i++) {
+        let mod = endTarget.items[i];
+        modTotal += mod.modifier
 
     }
-    
-function addMods(){
-    var modTotal = 0
-    for (let i = 0; i < endTarget.items.length; i++) {
-        return modTotal += endTarget.items[i].modifier  
-    }
+    return modTotal
 
 }
 
 //Constructs
-function Target (name, health){
+function Target(name, health) {
     this.name = name;
     this.health = health;
-    this.hits = 0; 
-    this.items= []
+    this.hits = 0;
+    this.items = []
 }
 
-function Item(name, modifier, description){
+function Item(name, modifier, description) {
     this.name = name;
     this.modifier = modifier;
     this.description = description;
 }
 
 
-//giveItemToTarget()
+//giveItemToTarget(rock)
 //addMods()
+
+// bug bug can use this to extract info from addmon funciton
+//var results = addMods()   then push results to something....
 
 update()
