b0195ee8cef829c1315f598297e49e0cf9d77531
Steve Oldfield <steveold1611@gmail.com>
2018-04-27 15:04:41 -0600

    completed step 4 part:2

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,36 +1,73 @@
-var health = 100
-var name1 = "wacky"
-var hits = 0
+// var health = 100
+// var name1 = "wacky"
+// var hits = 0
+
+
+var endTarget = new Target('Wacky', 100)
+
+var items = {
+    shield:new Item('Shield',0.3, "This is an awesome shield!"),
+    fist: new Item('fist of thunder', 10, 'will give +10 damage'),
+    rock: new Item('Rock', 3, 'just a simple but hard rock')
+}
 
 
 function slap(){
-    health--
-    hits++
+   // debugger
+    endTarget.health--
+    endTarget.hits++
     // alert(health)
     update()
 }
 
 function punch(){
-    health -= 5
-    hits += 1
+    endTarget.health -= 5
+    endTarget.hits += 1
  //   alert(health)
     update()
 }
 
 function kick(){
-    health -= 10
-    hits += 1
+    endTarget.health -= 10
+    endTarget.hits += 1
     // alert(health)
     update()
 }
 
 function update(){
     // @ts-ignore
-    document.getElementById('healthNum').innerText = health
+    document.getElementById('healthNum').innerText = endTarget.health
     // @ts-ignore
-    document.getElementById('hithNum').innerText = hits
-    document.getElementById('nameID').innerText = name1
+    document.getElementById('hithNum').innerText = endTarget.hits
+    document.getElementById('nameID').innerText = endTarget.name
 }
+
+function giveItemToTarget() {
+    //bug bug: hard coded shield for now
+    //BUG BUG: this could cause some issues later: KEEP EYE ON!!!!
+       endTarget.items.push(items.shield)
+
+    }
+    
+giveItemToTarget()
+
+//Constructs
+function Target (name, health){
+    this.name = name;
+    this.health = health;
+    this.hits = 0; 
+    this.items= []
+}
+
+function Item(name, modifier, description){
+    this.name = name;
+    this.modifier = modifier;
+    this.description = description;
+}
+
+
+
+
 
 
 
